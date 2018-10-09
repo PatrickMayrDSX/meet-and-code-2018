b7fc894a5fe16a010572c2efdd18c447113a6245
Patrick Bichler <patrick.bichler@runtastic.com>
2018-10-09 11:25:46 +0200

    Adding links + rank fallback (for avatars)

--- src/endpoints/coordinates/index.js
+++ src/endpoints/coordinates/index.js
@@ -1,25 +1,33 @@
-import { get, chunk } from 'lodash';
+import { get, isFinite, toNumber, chunk } from 'lodash';
 
 import readJSON from '../../util/readJSON';
 import CustomError from '../../util/CustomError';
 
-const PAGE_SIZE = 1;
+const PAGE_SIZE = 14;
 
 const _coordinatesFn = (queryParameter) => {
   const coordinatesJSON = readJSON(`${__dirname}/../../../fixtures/coordinates.json`);
 
   const allCoordinates = coordinatesJSON.coordinates;
-  const pageNumber = get(queryParameter, 'page', 1); // page-numbers start at 1!
+  const pageNumber = toNumber(get(queryParameter, 'page', 1)); // page-numbers start at 1!
   const pages = chunk(allCoordinates, PAGE_SIZE);
 
-  if (pages.length >= pageNumber) {
-    return {
-      type: 'json',
-      data: pages[pageNumber - 1],
-    };
+  if (!isFinite(pageNumber) || pageNumber < 1 || pageNumber > pages.length) {
+    throw new CustomError(400, 'invalid page number');
   }
 
-  throw new CustomError(400, 'invalid page number');
+  const links = {};
+  if (pageNumber < pages.length) {
+    links.next = `http://10.1.9.70:3001/coordinates?page=${pageNumber + 1}`;
+  }
+
+  return {
+    type: 'json',
+    data: {
+      coordinates: pages[pageNumber - 1],
+      links,
+    },
+  };
 }
 
 export default _coordinatesFn;
--- src/endpoints/routes/index.js
+++ src/endpoints/routes/index.js
@@ -1,13 +1,18 @@
-import { get, chunk } from 'lodash';
+import { toNumber, isFinite, get } from 'lodash';
 
 import readFile from '../../util/readFile';
 import CustomError from '../../util/CustomError';
 
 const _routesFN = (queryParameter) => {
-  const rank = get(queryParameter, 'rank');
-  if (rank == null) {
-    throw new CustomError(400, 'no rank provided');
+  let rank = toNumber(get(queryParameter, 'rank'));
+  if (!isFinite(rank) || rank < 1) {
+    throw new CustomError(400, 'Invalid rank number (or not provided)');
   }
+
+  if (rank > 5) {
+    rank = 'etc';
+  }
+
   return {
     data: readFile(`${__dirname}/../../../img/number_${rank}.png`, null),
     type: 'png',
